cca71ec2b0abe64b518bf96eba8f617d17c1e217
Administrator <Administrator@DBMS-PC>
2017-01-25 15:19:32 +0530

    data

---
+++ studentdetail/src/main/java/studentdetail/Test.java
@@ -0,0 +1,16 @@
+package studentdetail;
+
+public class Test {
+	public static void main(String args[]){
+		Student obj=new Student();
+		obj.setStudname("Karthi");
+		obj.setRegno(4008);
+		obj.setRollno(8);
+		StudDAO myDAO=new StudDAO();
+		myDAO.add(obj);
+		myDAO.update(11);
+
+		
+	}
+
+}
